5ef35bfeea497b6c1ac6976d7ff445f9f624d98c
Richard Smith <r.c.smith2011@gmail.com>
2014-09-14 03:15:03 -0700

    first commit of postcard project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var messageLabel: UILabel!
+    
+    @IBOutlet var fromLabel: UILabel!
+    
+    @IBOutlet var fromTextField: UITextField!
+    
+    @IBOutlet var messageTextField: UITextField!
+    
+    @IBOutlet var sendMailButton: UIButton!
+  
+    @IBAction func sendMailButtonPressed(sender: AnyObject) {
+        messageLabel.text = messageTextField.text
+        fromLabel.text = fromTextField.text
+        messageLabel.hidden = false
+        fromLabel.textColor = UIColor.blueColor()
+        messageLabel.textColor = UIColor.redColor()
+        fromLabel.hidden = false
+        sendMailButton.setTitle("sent mail", forState: UIControlState.Normal)
+        messageTextField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
